85141b0f9daf95677a0eac43fc8097e2135629da
jcanalesTIA <165836004+jcanalesTIA@users.noreply.github.com>
2024-07-14 16:29:49 +0200

    (feat) comment.service: Add comment service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommentService } from './services/comment.service';
+import { CommentModel } from './models/comment.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ToDo';
+  private readonly commentService = inject(CommentService);
+  comments: CommentModel[] = [];
+
+  constructor() {
+    console.log('11');
+  }
+
+  ngOnInit(): void {
+    this.commentService
+      .getComments()
+      .subscribe((comments) => (this.comments = comments));
+
+    console.log(this.comments);
+  }
 }
---
+++ src/app/services/comment.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { CommentModel } from '../models/comment.model';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root',
+})
+export class CommentService {
+  private readonly http = inject(HttpClient);
+  private apiUrl = 'https://jsonplaceholder.typicode.com';
+
+  getComments(): Observable<CommentModel[]> {
+    console.log('${this.apiUrl}/comments');
+    return this.http.get<CommentModel[]>(`${this.apiUrl}/comments`);
+  }
+
+  getCommentById(id: number): Observable<CommentModel> {
+    return this.http.get<CommentModel>(`${this.apiUrl}/comments/${id}`);
+  }
+
+  getCommentsByPostId(postId: number): Observable<CommentModel[]> {
+    return this.http.get<CommentModel[]>(
+      `${this.apiUrl}/posts/${postId}/comments`
+    );
+  }
+
+  createComment(comment: CommentModel): Observable<CommentModel> {
+    return this.http.post<CommentModel>(`${this.apiUrl}/comments`, comment);
+  }
+
+  updateComment(comment: CommentModel): Observable<CommentModel> {
+    return this.http.put<CommentModel>(
+      `${this.apiUrl}/comments/${comment.id}`,
+      comment
+    );
+  }
+
+  deleteComment(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/comments/${id}`);
+  }
+}
